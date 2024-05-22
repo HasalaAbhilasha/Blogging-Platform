76323a7c694a0266758eb29da453989a13d4cc82
HasalaAbhilasha <hasala200301abhilasha@gmail.com>
2024-05-22 21:50:09 +0530

    create template blog post

---
+++ client/src/App.jsx
@@ -0,0 +1,37 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">Blogging-Platform</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <img src="https://via.placeholder.com/150" alt="placeholder" />
+        <h2>Blog Title</h2>
+        <p>sid fsdifsidf sdfisudfsdf shdfisdif sidfisdfisd fisdbfusdhf uihs dfuisdh fiuhsduifhsduf sduifhbsdufh </p>
+        <a href="">Read More</a>
+      </div>
+      <div className="post">
+        <img src="https://via.placeholder.com/150" alt="placeholder" />
+        <h2>Blog Title</h2>
+        <p>sid fsdifsidf sdfisudfsdf shdfisdif sidfisdfisd fisdbfusdhf uihs dfuisdh fiuhsduifhsduf sduifhbsdufh </p>
+        <a href="">Read More</a>
+      </div>
+      <div className="post">
+        <img src="https://via.placeholder.com/150" alt="placeholder" />
+        <h2>Blog Title</h2>
+        <p>sid fsdifsidf sdfisudfsdf shdfisdif sidfisdfisd fisdbfusdhf uihs dfuisdh fiuhsduifhsduf sduifhbsdufh </p>
+        <a href="">Read More</a>
+      </div>
+    </main>
+  )
+}
+
+export default App
