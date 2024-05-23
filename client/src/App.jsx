efe95634c42940105d4c7ef1058278f6647012dd
HasalaAbhilasha <hasala200301abhilasha@gmail.com>
2024-05-23 10:54:43 +0530

    routing with react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,67 +1,23 @@
 
 import './App.css'
+import Post from './post';
+import Header from './Header';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './Layout';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
 
   return (
-    <main>
-      <header>
-        <a href="" className="logo">Blogging-Platform</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path={"/login"} element={<LoginPage />} />
+      </Route>
+    </Routes>
 
-      <div className="post">
-        <div className="image">
-          <img src="https://www.computerworld.com/wp-content/uploads/2024/05/G_usecase_04_enterprise_sales_brand_consistency_custservice_one_click_brand_apply.png?resize=1024%2C576&quality=50&strip=all" alt="placeholder" />
-        </div>
-        <div className="texts">
-          <h2>Adobe Express Enterprise is where iWork could boldly go</h2>
-          <p className="info">
-            <span>Author: John Doe</span>
-            <time>2023-01-06 16:45</time>
-          </p>
-          <p className='summary'>Apple’s deliberate approach to generative AI (genAI) means other companies have been rolling out solutions that include the quickly evolving technology — and Adobe’s pushing hard to realize its benefits. GenAI already figures in its creative products, and as of today it’s available in Adobe Express for Enterprise.
-
-            I can’t help but feel that genAI-driven tools like these would make excellent additions across the Apple iWork suite (Numbers, Pages, Keynote). Think how useful it would be to be able to sketch out ideas on iPhone to improve on other Apple devices, or even using a PC and iCloud.com. Introduction of powerful creative tools like these with every Mac, iPhone, or iPad makes plenty of sense, and most Apple users surely hope for something close to this at WWDC. </p>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="image">
-          <img src="https://www.computerworld.com/wp-content/uploads/2024/05/G_usecase_04_enterprise_sales_brand_consistency_custservice_one_click_brand_apply.png?resize=1024%2C576&quality=50&strip=all" alt="placeholder" />
-        </div>
-        <div className="texts">
-          <h2>Adobe Express Enterprise is where iWork could boldly go</h2>
-          <p className="info">
-            <span>Author: John Doe</span>
-            <time>2023-01-06 16:45</time>
-          </p>
-          <p className='summary'>Apple’s deliberate approach to generative AI (genAI) means other companies have been rolling out solutions that include the quickly evolving technology — and Adobe’s pushing hard to realize its benefits. GenAI already figures in its creative products, and as of today it’s available in Adobe Express for Enterprise.
-
-            I can’t help but feel that genAI-driven tools like these would make excellent additions across the Apple iWork suite (Numbers, Pages, Keynote). Think how useful it would be to be able to sketch out ideas on iPhone to improve on other Apple devices, or even using a PC and iCloud.com. Introduction of powerful creative tools like these with every Mac, iPhone, or iPad makes plenty of sense, and most Apple users surely hope for something close to this at WWDC. </p>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="image">
-          <img src="https://www.computerworld.com/wp-content/uploads/2024/05/G_usecase_04_enterprise_sales_brand_consistency_custservice_one_click_brand_apply.png?resize=1024%2C576&quality=50&strip=all" alt="placeholder" />
-        </div>
-        <div className="texts">
-          <h2>Adobe Express Enterprise is where iWork could boldly go</h2>
-          <p className="info">
-            <span>Author: John Doe</span>
-            <time>2023-01-06 16:45</time>
-          </p>
-          <p className='summary'>Apple’s deliberate approach to generative AI (genAI) means other companies have been rolling out solutions that include the quickly evolving technology — and Adobe’s pushing hard to realize its benefits. GenAI already figures in its creative products, and as of today it’s available in Adobe Express for Enterprise.
-
-            I can’t help but feel that genAI-driven tools like these would make excellent additions across the Apple iWork suite (Numbers, Pages, Keynote). Think how useful it would be to be able to sketch out ideas on iPhone to improve on other Apple devices, or even using a PC and iCloud.com. Introduction of powerful creative tools like these with every Mac, iPhone, or iPad makes plenty of sense, and most Apple users surely hope for something close to this at WWDC. </p>
-        </div>
-      </div>
-    </main>
-  )
+  );
 }
 
 export default App
