3a349b23c4ca860fd015e10fcc52448f40e00a2d
HasalaAbhilasha <hasala200301abhilasha@gmail.com>
2024-05-26 20:24:47 +0530

    chore: Update server listen address to use '0.0.0.0' and include the port number in the console log

--- api/index.js
+++ api/index.js
@@ -30,6 +30,6 @@ app.use(blogRoutes);
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-    console.log('Server is running on port 4000');
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
 });
